62da348b62b4ef1e8c2ae7cbf097c0df3b383f99
yamboni1 <ncyambot.work@gmail.com>
2023-08-26 12:47:46 +0800

    feat: change the findbyId to implement the employeeresponse

--- src/main/java/com/afs/restapi/service/EmployeeService.java
+++ src/main/java/com/afs/restapi/service/EmployeeService.java
@@ -25,9 +25,10 @@ public class EmployeeService {
         return employeeRepository.findAll();
     }
 
-    public Employee findById(Long id) {
-        return employeeRepository.findById(id)
+    public EmployeeResponse findById(Long id) {
+        Employee employee = employeeRepository.findById(id)
                 .orElseThrow(EmployeeNotFoundException::new);
+        return EmployeeMapper.toResponse(employee);
     }
 
     public void update(Long id, Employee employee) {
