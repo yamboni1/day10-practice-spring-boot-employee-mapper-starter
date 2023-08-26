f620e29f327e2ffc15348febfa2c38f6b8469cef
yamboni1 <ncyambot.work@gmail.com>
2023-08-26 14:37:54 +0800

    refactor remove unused constructor

--- src/main/java/com/afs/restapi/service/dto/EmployeeResponse.java
+++ src/main/java/com/afs/restapi/service/dto/EmployeeResponse.java
@@ -42,12 +42,6 @@ public class EmployeeResponse {
     public EmployeeResponse(){
 
     }
-    public EmployeeResponse(Long id, String name, Integer age, String gender) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
 
 
 
