c4f3062b3423acebc44cfabfae549a79fc887725
yamboni1 <ncyambot.work@gmail.com>
2023-08-26 12:51:54 +0800

    feat: add constructor for companyId

--- src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
+++ src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
@@ -3,6 +3,21 @@ package com.afs.restapi.service.dto;
 public class CompanyRequest {
     private String name;
 
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public CompanyRequest( Long companyId, String name) {
+        this.name = name;
+        this.companyId = companyId;
+    }
+
+    private Long companyId;
+
     public void setName(String name) {
         this.name = name;
     }
