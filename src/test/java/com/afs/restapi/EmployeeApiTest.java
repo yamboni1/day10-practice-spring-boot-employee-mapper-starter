75df3cc923904a4013434969d27e0f3f334ca7a9
yamboni1 <ncyambot.work@gmail.com>
2023-08-26 12:50:09 +0800

    test: should_find_employee_by_id implementation to employeerequest and employeeresponse

--- src/test/java/com/afs/restapi/EmployeeApiTest.java
+++ src/test/java/com/afs/restapi/EmployeeApiTest.java
@@ -99,15 +99,16 @@ class EmployeeApiTest {
 
     @Test
     void should_find_employee_by_id() throws Exception {
-        Employee employee = employeeRepository.save(getEmployeeBob());
-
-        mockMvc.perform(get("/employees/{id}", employee.getId()))
+        EmployeeRequest employeeRequest = new EmployeeRequest("Alice", 24, "Female", 8000, null);
+        Employee employee = EmployeeMapper.toEntity(employeeRequest);
+        EmployeeResponse employeeResponse = EmployeeMapper.toResponse(employeeRepository.save(employee));
+        mockMvc.perform(get("/employees/{id}", employeeResponse.getId()))
                 .andExpect(MockMvcResultMatchers.status().is(200))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(employee.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(employee.getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(employee.getAge()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value(employee.getGender()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(employee.getSalary()));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(employeeResponse.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(employeeResponse.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(employeeResponse.getAge()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value(employeeResponse.getGender()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.salary").doesNotExist());
     }
 
     @Test
